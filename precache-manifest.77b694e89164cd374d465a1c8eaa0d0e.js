666b029f82a95d4312771aac6290abaaf31c005d
Traian Stanciu <traian.stanciu@3pillarglobal.com>
2020-04-09 21:43:51 +0300

    Updates

---
+++ precache-manifest.77b694e89164cd374d465a1c8eaa0d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a238a837420214965a23d3e1243a38",
+    "url": "/train-game/index.html"
+  },
+  {
+    "revision": "1dc4c3c823f415bd6f7d",
+    "url": "/train-game/static/css/main.27d013b1.chunk.css"
+  },
+  {
+    "revision": "2d2f4c614fa048356572",
+    "url": "/train-game/static/js/2.02da747d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/train-game/static/js/2.02da747d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dc4c3c823f415bd6f7d",
+    "url": "/train-game/static/js/main.951ffbde.chunk.js"
+  },
+  {
+    "revision": "ae10caeee4ca2d845f1c",
+    "url": "/train-game/static/js/runtime~main.86114eda.js"
+  }
+]);
