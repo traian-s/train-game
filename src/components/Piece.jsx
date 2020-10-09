587241567daa32b3f36e6fabdaaea4573eb4fdd3
traian-s <stanciu.traian@gmail.com>
2020-10-09 21:26:49 +0300

    refactor Piece to functional with useReducer

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,96 +1,108 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useReducer } from 'react';
+
 import PIECES from 'constants/assetsMap';
 import { PIECE_TYPES } from 'constants/pieces';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUndo, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-export class Piece extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allowRotate: false,
-      rotate: props.rotate ? props.rotate : 0,
-      tempType: props.type,
-      tempRotate: props.rotate ? props.rotate : 0,
-      wheel: false
-    };
+const actionTypes = {
+  SET_ALLOW_ROTATE: 'SET_ALLOW_ROTATE',
+  SET_TYPE: 'SET_TYPE',
+  SET_ROTATE: 'SET_ROTATE',
+  SHOW_SELECTION_WHEEL: 'SHOW_SELECTION_WHEEL',
+  HIDE_SELECTION_WHEEL: 'HIDE_SELECTION_WHEEL'
+};
+
+const reducer = (state, action) => {
+  console.log(`${action.type} called with payload: ${action.payload}`);
+  switch (action.type) {
+    case actionTypes.SET_ALLOW_ROTATE:
+      return { ...state, allowRotate: action.payload };
+    case actionTypes.SET_TYPE:
+      return { ...state, internalType: action.payload };
+    case actionTypes.SET_ROTATE:
+      return { ...state, internalRotate: action.payload };
+    case actionTypes.SHOW_SELECTION_WHEEL:
+      return { ...state, selectionWheel: true };
+    case actionTypes.HIDE_SELECTION_WHEEL:
+      return { ...state, selectionWheel: false };
   }
+};
 
-  showWheel = () => this.setState({ wheel: true });
+const Piece = ({ rotate, type, stage, x, y, setPiece, makeMove }) => {
+  const [{ allowRotate, internalType, internalRotate, selectionWheel }, dispatch] = useReducer(
+    reducer,
+    {
+      allowRotate: false,
+      internalType: type,
+      internalRotate: rotate ? rotate : 0,
+      selectionWheel: false
+    }
+  );
 
-  hideWheel = () => this.setState({ wheel: false });
+  const showWheel = () => dispatch({ type: actionTypes.SHOW_SELECTION_WHEEL });
 
-  setSelection = type =>
-    this.setState({ tempType: type, allowRotate: PIECE_TYPES[type].allowRotate });
+  const hideWheel = () => dispatch({ type: actionTypes.HIDE_SELECTION_WHEEL });
 
-  saveSelection = () => {
-    this.hideWheel();
-    this.props.setPiece(this.props.x, this.props.y, this.state.tempType, this.state.tempRotate);
-    this.props.makeMove();
+  const setSelection = type => {
+    dispatch({ type: actionTypes.SET_TYPE, payload: type });
+    dispatch({ type: actionTypes.SET_ALLOW_ROTATE, payload: PIECE_TYPES[type].allowRotate });
   };
 
-  setRotation = rotation => {
-    this.setState(state => ({
-      tempRotate: Math.abs(state.tempRotate + rotation) === 360 ? 0 : state.tempRotate + rotation
-    }));
+  const saveSelection = () => {
+    hideWheel();
+    setPiece(x, y, internalType, internalRotate);
+    makeMove();
   };
 
-  render() {
-    const {
-      props: { type, rotate, stage },
-      state: { tempType, tempRotate, wheel, allowRotate }
-    } = this;
+  const setRotation = rotation => {
+    dispatch({
+      type: actionTypes.SET_ROTATE,
+      payload: Math.abs(internalRotate + rotation) === 360 ? 0 : internalRotate + rotation
+    });
+  };
 
-    return (
-      <div className={`piece piece-${type} ${wheel ? 'show-wheel' : ''}`}>
-        <div className={'selection-wheel'}>
-          {Object.keys(PIECES)
-            .filter(piece => stage.piece.includes(piece))
-            .map((key, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className={`quadrant quadrant-${idx}`}
-                  onClick={() => this.setSelection(key)}
-                >
-                  <img src={PIECES[key]} />
-                </div>
-              );
-            })}
-          <div className={'selection-preview'} onClick={() => this.saveSelection()}>
-            {tempType && <img className={`rotate-${tempRotate}`} src={PIECES[tempType]} />}
-          </div>
-          {allowRotate && (
-            <>
-              <div className={'selection-rotate-right'} onClick={() => this.setRotation(90)}>
-                <FontAwesomeIcon icon={faUndo} />
+  return (
+    <div className={`piece piece-${type} ${selectionWheel ? 'show-wheel' : ''}`}>
+      <div className={'selection-wheel'}>
+        {Object.keys(PIECES)
+          .filter(piece => stage.piece.includes(piece))
+          .map((key, idx) => {
+            return (
+              <div
+                key={idx}
+                className={`quadrant quadrant-${idx}`}
+                onClick={() => setSelection(key)}
+              >
+                <img src={PIECES[key]} />
               </div>
-              <div className={'selection-rotate-left'} onClick={() => this.setRotation(-90)}>
-                <FontAwesomeIcon icon={faUndo} />
-              </div>
-            </>
+            );
+          })}
+        <div className={'selection-preview'} onClick={() => saveSelection()}>
+          {internalType && (
+            <img className={`rotate-${internalRotate}`} src={PIECES[internalType]} />
           )}
-          <div className={'selection-close'} onClick={() => this.hideWheel()}>
-            <FontAwesomeIcon icon={faTimes} />
-          </div>
         </div>
-        <div className={'piece-image'} onClick={() => this.showWheel()}>
-          {type && <img className={`rotate-${rotate}`} src={PIECES[type]} />}
+        {allowRotate && (
+          <>
+            <div className={'selection-rotate-right'} onClick={() => setRotation(90)}>
+              <FontAwesomeIcon icon={faUndo} />
+            </div>
+            <div className={'selection-rotate-left'} onClick={() => setRotation(-90)}>
+              <FontAwesomeIcon icon={faUndo} />
+            </div>
+          </>
+        )}
+        <div className={'selection-close'} onClick={() => hideWheel()}>
+          <FontAwesomeIcon icon={faTimes} />
         </div>
       </div>
-    );
-  }
-}
-
-Piece.propTypes = {
-  x: PropTypes.number,
-  y: PropTypes.number,
-  type: PropTypes.string,
-  rotate: PropTypes.number,
-  setPiece: PropTypes.func,
-  makeMove: PropTypes.func,
-  stage: PropTypes.object
+      <div className={'piece-image'} onClick={() => showWheel()}>
+        {type && <img className={`rotate-${rotate}`} src={PIECES[type]} />}
+      </div>
+    </div>
+  );
 };
 
 export default Piece;
