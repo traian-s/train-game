e4b0f5b571907305bf261ae23c34065b275ce1c0
traian-s <stanciu.traian@gmail.com>
2020-10-09 21:26:59 +0300

    refactor Board to functional

--- src/views/Board.jsx
+++ src/views/Board.jsx
@@ -1,60 +1,53 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { initBoard, setPiece } from 'store/actions/board';
 import { initPlayers, playerMakeMove } from 'store/actions/game';
 import Piece from 'components/Piece';
 
-class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: []
-    };
-  }
-
-  componentDidMount() {
-    const {
-      props: { initBoard, initPlayers }
-    } = this;
-
+const Board = ({
+  board,
+  rows,
+  columns,
+  stage,
+  setPiece,
+  playerMakeMove,
+  initBoard,
+  initPlayers
+}) => {
+  useEffect(() => {
     initBoard();
     initPlayers();
-  }
+  }, []);
 
-  render() {
-    const {
-      props: { board, rows, columns, setPiece, playerMakeMove, stage }
-    } = this;
-    return (
-      <div className={'game-board'} style={{ width: `calc(80px * ${columns})` }}>
-        {board.map((row, i) =>
-          row.map((cell, j) => {
-            return (
-              <div
-                className={`cell 
+  return (
+    <div className={'game-board'} style={{ width: `calc(80px * ${columns})` }}>
+      {board.map((row, i) =>
+        row.map((cell, j) => {
+          return (
+            <div
+              className={`cell 
                 ${j === columns - 1 ? 'right-edge' : ''} 
                 ${i === rows - 1 ? 'bottom-edge' : ''}`}
-                style={{ flex: `calc(100% / ${columns} - 1px) 0 0` }}
-                key={`cell-${i}${j}`}
-              >
-                <Piece
-                  x={i}
-                  y={j}
-                  type={cell.type}
-                  rotate={cell.rotation}
-                  setPiece={setPiece}
-                  makeMove={playerMakeMove}
-                  stage={stage}
-                />
-              </div>
-            );
-          })
-        )}
-      </div>
-    );
-  }
-}
+              style={{ flex: `calc(100% / ${columns} - 1px) 0 0` }}
+              key={`cell-${i}${j}`}
+            >
+              <Piece
+                x={i}
+                y={j}
+                type={cell.type}
+                rotate={cell.rotation}
+                setPiece={setPiece}
+                makeMove={playerMakeMove}
+                stage={stage}
+              />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   board: state.board.map,
