db2d8140a9d6314e2a294a077772d6dea64ebee8
traian-s <stanciu.traian@gmail.com>
2020-10-10 13:20:04 +0300

    fix rotations bug

--- src/utils/gameHelpers.js
+++ src/utils/gameHelpers.js
@@ -1,11 +1,12 @@
 // export const isMatch = (pieceA, pieceB) => {};
 
 export const rotateConnections = (connections, rotation) => {
+  const connectionsCopy = [...connections];
   const reverse = rotation < 0;
   const times = Math.abs(rotation / 90);
   for (let i = 0; i < times; i++) {
-    if (reverse) connections.unshift(connections.pop());
-    else connections.push(connections.shift());
+    if (reverse) connectionsCopy.unshift(connectionsCopy.pop());
+    else connectionsCopy.push(connectionsCopy.shift());
   }
-  return connections;
+  return connectionsCopy;
 };
